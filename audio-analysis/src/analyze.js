5ec535be19a4af56ed01e747ac1c67b13cefaf9f
Jake Cheek <jake@f-m.fm>
2020-02-29 10:53:06 -0500

    create note visualization

--- audio-analysis/src/analyze.js
+++ audio-analysis/src/analyze.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-bitwise */
 /* eslint-disable no-console */
 /* eslint-disable no-param-reassign */
 import 'p5/lib/addons/p5.sound'
@@ -5,6 +6,7 @@ import 'p5/lib/addons/p5.sound'
 const sketch = soundFile => p => {
   let sound
   let dl
+  let notes
 
   function getBeats() {
     return new Promise(resolve => {
@@ -29,7 +31,7 @@ const sketch = soundFile => p => {
         const rand = p.noise(i * noiseScale)
         console.log(rand)
         if (rand < noteChance) {
-          const sideRand = p.map(rand, 0, noteChance, 0, 1)
+          const sideRand = p.random()
           const heightRand = p.noise(i * noiseScale + 10000)
           if (sideRand < leftChance) {
             note = [heightRand, false]
@@ -64,15 +66,59 @@ const sketch = soundFile => p => {
     return dlLink
   }
 
+  function createSeed(str) {
+    let hash = 0
+    let i
+    let chr
+    if (str.length === 0) return hash
+    for (i = 0; i < str.length; i += 1) {
+      chr = str.charCodeAt(i)
+      hash = (hash << 5) - hash + chr
+      hash |= 0 // Convert to 32bit integer
+    }
+    return hash
+  }
+
   p.preload = () => {
+    const seed = createSeed(soundFile)
+    p.noiseSeed(seed)
+    p.randomSeed(seed)
     sound = p.loadSound(soundFile)
   }
 
   p.setup = async () => {
     const beats = await getBeats()
-    const notes = createNoteMap(beats)
-    console.log(notes)
+    notes = createNoteMap(beats)
     createDownloadLink(notes)
+    p.createCanvas(p.windowWidth, 100)
+  }
+
+  p.draw = () => {
+    if (!notes) return
+    if (!sound.isPlaying()) sound.play()
+    p.background(255)
+    Object.entries(notes).forEach(([timestamp, note]) => {
+      const x = p.map(timestamp, 0, sound.duration(), 0, p.width)
+      const y = p.map(
+        note.find(n => !!n),
+        0,
+        1,
+        0,
+        p.height
+      )
+      const color =
+        // eslint-disable-next-line no-nested-ternary
+        note[0] && note[1]
+          ? p.color(255, 0, 255)
+          : note[0]
+          ? p.color(0, 0, 255)
+          : p.color(255, 0, 0)
+      p.fill(color)
+      p.square(x, y, 10)
+    })
+
+    const pos = p.map(sound.currentTime(), 0, sound.duration(), 0, p.width)
+    p.line(pos, 0, pos, p.height)
   }
 }
 
