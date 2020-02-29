457f097e07d462f7dcc2756bcd2881b42a680567
Jake Cheek <jake@f-m.fm>
2020-02-29 09:41:32 -0500

    add download for generated notes

--- audio-analysis/src/analyze.js
+++ audio-analysis/src/analyze.js
@@ -4,6 +4,7 @@ import 'p5/lib/addons/p5.sound'
 
 const sketch = soundFile => p => {
   let sound
+  let dl
 
   function getBeats() {
     return new Promise(resolve => {
@@ -19,21 +20,23 @@ const sketch = soundFile => p => {
     const leftChance = 0.45
     const rightChance = 0.45
     const bothChance = 0.1
+    const noiseScale = 0.2
+    p.noiseDetail(8, 0.5)
     return Object.assign(
       {},
       ...beats.map((timestamp, i) => {
         let note
-        const rand = p.noise(i)
+        const rand = p.noise(i * noiseScale)
+        console.log(rand)
         if (rand < noteChance) {
-          if (rand < leftChance * noteChance) {
-            note = [true, false]
-          } else if (rand < (leftChance + rightChance) * noteChance) {
-            note = [false, true]
-          } else if (
-            rand <
-            (leftChance + rightChance + bothChance) * noteChance
-          ) {
-            note = [true, true]
+          const sideRand = p.map(rand, 0, noteChance, 0, 1)
+          const heightRand = p.noise(i * noiseScale + 10000)
+          if (sideRand < leftChance) {
+            note = [heightRand, false]
+          } else if (sideRand < leftChance + rightChance) {
+            note = [false, heightRand]
+          } else if (sideRand < leftChance + rightChance + bothChance) {
+            note = [heightRand, heightRand]
           }
           return { [timestamp]: note }
         }
@@ -42,16 +45,31 @@ const sketch = soundFile => p => {
     )
   }
 
+  function createDownloadUrl(data) {
+    const blob = new Blob([JSON.stringify(data)], { type: 'application/json' })
+    if (dl !== null) window.URL.revokeObjectURL(dl)
+    dl = window.URL.createObjectURL(blob)
+    return dl
+  }
+
+  function createDownloadLink(data) {
+    const link = document.createElement('a')
+    link.textContent = 'Download'
+    link.setAttribute('download', 'notes.json')
+    link.href = createDownloadUrl(data)
+    document.body.appendChild(link)
+    return link
+  }
+
   p.preload = () => {
     sound = p.loadSound(soundFile)
   }
 
   p.setup = async () => {
-    p.createCanvas(p.windowWidth, p.windowHeight - 10)
-
     const beats = await getBeats()
     const notes = createNoteMap(beats)
     console.log(notes)
+    createDownloadLink(notes)
   }
 }
 
